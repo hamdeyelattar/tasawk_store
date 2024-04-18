258e34c99755d5c110646c65488f5ddba654363f
Hamdy <hamdey.elattar@gmail.com>
2024-04-18 22:03:34 +0200

    tasawk store

---
+++ android/app/src/main/kotlin/com/example/tasawk/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tasawk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tasawk/tasawk_app.dart';
+
+void main() {
+  runApp(const Tasawk());
+}
